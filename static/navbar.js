2801d2c77c1d14c9b50691ccaf643ccd28d2809a
rvid <github@rvid.eu>
2025-08-17 22:58:42 +0200

    Start at development of mobile support

--- static/navbar.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-  fetch('/static/navbar.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('navbar-container').innerHTML = data;
-    })
-};
